cb5b54a4cdc642d6b95edc8f048e4355f079009d
Serhii-Polishchuk <serhii.polishchuk@skyhive.io>
2025-08-16 18:23:32 +0300

    remove unnecessary code fragments

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,3 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import { filtersReducer } from './filtersSlice';
-// import { contactsReducer } from './contactsSlice';
-
-// const store = configureStore({
-//   reducer: {
-//     contacts: contactsReducer,
-//     filters: filtersReducer,
-//   },
-// });
-
 import { configureStore } from '@reduxjs/toolkit';
 import {
   persistStore,
